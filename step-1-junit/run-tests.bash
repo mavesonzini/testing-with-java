23d65067ce77580337996a11abdfb020ec96adac
María Verónica Sonzini <mavesonzini@gmail.com>
2017-06-07 11:53:11 +0100

    Secont test working

--- step-1-junit/run-tests.bash
+++ step-1-junit/run-tests.bash
@@ -3,6 +3,7 @@
 PWD=$(pwd)
 # NOTE: the following env var is for use with git bash on Windows installations only,
 # and will vary according to the version of the JDK you installed:
+
 export PATH="${PATH}:/c/Program Files/Java/jdk1.8.0_131/bin"
 export CLASSPATH=.:${PWD}/src:${PWD}/test:test/junit-4.12.jar:test/hamcrest-core-1.3.jar
 
