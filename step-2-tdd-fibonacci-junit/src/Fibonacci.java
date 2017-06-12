efe9980131190dbe5e36f4ba24edd85af159699d
María Verónica Sonzini <mavesonzini@gmail.com>
2017-06-12 10:34:01 +0100

    Add step 2 code

--- step-2-tdd-fibonacci-junit/src/Fibonacci.java
+++ step-2-tdd-fibonacci-junit/src/Fibonacci.java
@@ -1,2 +1,8 @@
 public class Fibonacci {
+    public int calculate(int index) {
+        if (index == 0 || index == 1){
+            return 1;
+        }
+        return calculate(index - 1) + calculate(index - 2);
+    }
 }
--- step-2-tdd-fibonacci-junit/test/FibonacciTest.java
+++ step-2-tdd-fibonacci-junit/test/FibonacciTest.java
@@ -1,2 +1,52 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
 public class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @Before
+    public void setUp() {
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    public void calculate_0() {
+        int testIndex = 0;
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    }
+
+    @Test
+    public void calculate_1() {
+        int testIndex = 1;
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    }
+
+    @Test
+    public void calculate_6() {
+        int testIndex = 6;
+        int expectedResult = 13;
+
+        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    }
+
+    @Test
+    public void calculate_20() {
+        int testIndex = 20;
+        int expectedResults = 10946;
+
+        assertEquals(expectedResults, fibonacci.calculate(testIndex));
+    }
+//
+//    @Test
+//    public void calculate_negative() {
+//        int testIndex = -5;
+//        int expectedResults = 0;
+//
+//        assertEqual(expectedResults, fibonacci.calculate(testIndex));
+//    }
 }
